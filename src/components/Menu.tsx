eca235921622b10583517bbee4b80947566265a1
Incredible noble <tumson1000000@gmail.com>
2024-09-05 18:59:22 +0100

    first commit

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,58 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    // <div className="mt-4 text-sm">
+    //   {menuItems.map((item) => (
+    //     <div key={item.title} className="flex flex-col gap-2">
+    //       <span className="hidden lg-block text-gray-400 font-light my-4">
+    //         {item.title}
+    //       </span>
+    //       <ul>
+    //         {item.items.map((subItem) => (
+    //           <Link
+    //             key={subItem.label}
+    //             href={subItem.href}
+    //             className="flex items-center justify-center lg:justify-start gap-4 lg:gap-6 text-gray-500 py-2"
+    //           >
+    //             <Image
+    //               src={subItem.icon}
+    //               alt={subItem.label}
+    //               width={20}
+    //               height={20}
+    //             />
+    //             <span className="hidden lg:block">{subItem.label}</span>
+    //           </Link>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   ))}
+    // </div>
+
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            return (
+              <Link
+                href={item.href}
+                key={item.label}
+                className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+              >
+                <Image src={item.icon} alt="" width={20} height={20} />
+                <span className="hidden lg:block">{item.label}</span>
+              </Link>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
