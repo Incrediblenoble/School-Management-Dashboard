c22a7e460aa993b70a28fde39a3df290a29388e9
Incredible noble <tumson1000000@gmail.com>
2024-09-06 20:20:11 +0100

    student page implemented

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -915,14 +915,25 @@ export const announcementsData = [
   },
 ];
 
-
 // YOU SHOULD CHANGE THE DATES OF THE EVENTS TO THE CURRENT DATE TO SEE THE EVENTS ON THE CALENDAR
 export const calendarEvents = [
   {
     title: "Math",
     allDay: false,
-    start: new Date(2024, 7, 12, 8, 0),
-    end: new Date(2024, 7, 12, 8, 45),
+    start: new Date(2024, 2, 18, 10, 0, 0),
+    end: new Date(2024, 2, 18, 11, 0, 0),
+  },
+  {
+    title: "English",
+    allDay: false,
+    start: new Date(2024, 6, 12, 9, 0, 0),
+    end: new Date(2024, 6, 12, 9, 45, 0),
+  },
+  {
+    title: "Math",
+    allDay: false,
+    start: new Date(2025, 9, 12, 8, 0),
+    end: new Date(2025, 9, 12, 8, 45),
   },
   {
     title: "English",
